5a643a47e0cf531f3ef8153b97a65ac9bf9f70d2
Kawin Viriyaprasopsook <kawin.v@kkumail.com>
2020-01-29 10:28:27 +0700

    Update context.go

--- context.go
+++ context.go
@@ -14,6 +14,8 @@ type Context struct {
 	echo.Context
 }
 
+var _ echo.Context = &Context{}
+
 // Redirect redirects the request to a provided URL with status code.
 // By this order
 // 1.HTTP redirect
